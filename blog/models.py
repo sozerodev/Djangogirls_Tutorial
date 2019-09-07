26332bf800c34c762824330a6894c374efe59cfc
sozerodev <sozerodev@gmail.com>
2019-09-07 16:20:14 +0900

    Add view to remove blog post

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,16 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
-path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
+    # ㅇㅣ전 버전에서는
+    # url(r'^drafts/$', views.post_draft_list, name='post_draft_list'), 이렇게 썼었음.
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
+
+
+
+
 
